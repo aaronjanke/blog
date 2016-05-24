78677561d65f4543b65f48bf70da551da599ade7
janaaron97 <aaron@deepspacerobots.com>
2016-05-24 22:37:45 +0000

    Initial Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article Successfully Created"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article Successfully Updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+    
+    private
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
+    
+end
