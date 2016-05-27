57a596eeda433d77658e932c127f8fe0b873bc3c
janaaron97 <aaron@deepspacerobots.com>
2016-05-27 23:47:49 +0000

    Bootstrap added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,39 @@
 class ArticlesController < ApplicationController
-    
-    def index
-        @articles = Article.all
+
+    def new
+        @article = Article.new
     end
     
     def show
-        @article = Article.find(params[:id])
+       @article = Article.find(params[:id]) 
     end
     
     def create
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article Successfully Created"
+            flash[:notice] = "Article Saved Successfully!"
             redirect_to articles_path
         else
             render 'new'
         end
     end
     
-    def new
-        @article = Article.new
+    def index
+        @articles = Article.all
+    end 
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.save(article_params)
+            flash[:notice] = "Article Updated"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
     end
     
     def destroy
@@ -28,23 +42,13 @@ class ArticlesController < ApplicationController
         redirect_to articles_path
     end
     
-    def edit
-        @article = Article.find(params[:id])
-    end
-    
-    def update
-        @article = Article.find(params[:id])
-        if @article.update(article_params)
-            flash[:notice] = "Article Successfully Updated"
-            redirect_to articles_path
-        else
-            render 'edit'
-        end
-    end
-    
     private
-        def article_params
-            params.require(:article).permit(:title, :description)
-        end
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
     
+
+
+
+
 end
